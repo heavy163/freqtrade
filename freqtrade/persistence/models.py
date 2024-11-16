dbacb7ac6f02682c0d76022b5aa029e869813c19
heavy <heavy@163.com>
2024-11-16 20:04:05 +0800

    [Update] db session init config, remove reuse of trade session by posistion records, which will created by http request, and should release after request done.

--- freqtrade/persistence/models.py
+++ freqtrade/persistence/models.py
@@ -97,7 +97,7 @@ def init_db(db_url: str) -> None:
         sessionmaker(bind=engine, autoflush=True), scopefunc=get_request_or_thread_id
     )
     FtWalletRecord.session = FtPrediction.session
-    FtPostionRecord.session = FtPostion.session
+    FtPostionRecord.session = FtPrediction.session
     _KeyValueStoreModel.session = Trade.session
     _CustomData.session = scoped_session(
         sessionmaker(bind=engine, autoflush=True), scopefunc=get_request_or_thread_id
