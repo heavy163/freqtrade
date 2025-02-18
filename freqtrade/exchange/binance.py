0de343f5d6c5ed2c4b2253fa1f2bc4f11be3303a
heavy <heavy@163.com>
2025-02-18 21:18:29 +0800

    [Enable] websocket for future on binance.

--- freqtrade/exchange/binance.py
+++ freqtrade/exchange/binance.py
@@ -47,7 +47,7 @@ class Binance(Exchange):
             PriceType.LAST: "CONTRACT_PRICE",
             PriceType.MARK: "MARK_PRICE",
         },
-        "ws_enabled": False,
+        "ws_enabled": True,
     }
 
     _supported_trading_mode_margin_pairs: list[tuple[TradingMode, MarginMode]] = [
